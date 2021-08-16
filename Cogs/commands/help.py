e9fd0d0a3ce83221ec113c1febb3ed1bd03dbb89
Ignis <gaemingviper@gmail.com>
2021-08-16 11:38:34 +0800

    Added Help Command

---
+++ Cogs/commands/help.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+import discord
+
+
+class Help(commands.Cog):  # TODO: Create Paginator for each command
+
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @commands.group(invoke_without_command=True)
+    async def help(self, ctx: commands.Context):
+        pfx = ctx.prefix
+        # main fields
+        __fields = {
+            f'{pfx}search': 'Command for searching information about Toram Items',
+        }
+        __admin_fields = {
+        }
+
+        if ctx.author.guild_permissions.administrator:
+            __fields.update(__admin_fields)
+
+        embed = discord.Embed()  # TODO: Improve Help Embed
+        embed.set_author(name='Help')
+        for name, value in __fields.items():
+            embed.add_field(name=name, value=value, inline=False)
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Help(bot))
